7946bc6062071f063ed93afabe62095ce222d4c0
zeref <krishh16@yahoo.com>
2024-04-20 12:59:54 +0530

    added backend server

---
+++ apps/server/src/index.ts
@@ -0,0 +1,13 @@
+import http from 'http';
+
+async function init() {
+    const httpServer = http.createServer();
+    const PORT = process.env.PORT ? process.env.PORT : 8000;
+    
+    httpServer.listen(PORT, () => {
+        console.log(`HTTP Server started at port ${PORT}`)
+    }) 
+}
+
+
+init();
